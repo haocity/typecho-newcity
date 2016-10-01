b0df6e42d8485ced61ee44299b6895af9ddc3b3a
皓东haocity <ureygt@gmail.com>
2016-10-01 19:23:34 +0800

    加入qjax代码

--- footer.php
+++ footer.php
@@ -17,7 +17,19 @@
          	
          </div>
 </footer>
-<script src="<?php $this->options->themeUrl('nav.js'); ?>" type="text/javascript" charset="utf-8"></script>
 <?php $this->footer(); ?>
+	<script type="text/javascript">
+ $.pjax({
+        selector: 'a',
+        container: '#container', //内容替换的容器
+        show: 'fade',  //展现的动画，支持默认和fade, 可以自定义动画方式，这里为自定义的function即可。
+        cache: true,  //是否使用缓存
+        storage: true,  //是否使用本地存储
+        titleSuffix: '', //标题后缀
+        filter: function(){},
+        callback: function(){pajx_loadDuodsuo();}
+    })
+
+	</script>
 </body>
 </html>
