287eebfa79e580d1590c52954e7b47ba7db1e9a6
疯狂减肥带 <ureygt@gmail.com>
2016-10-08 14:51:22 +0800

    pjax代码修改

--- footer.php
+++ footer.php
@@ -19,16 +19,17 @@
 </footer>
 <?php $this->footer(); ?>
 	<script type="text/javascript">
- $.pjax({
-        selector: 'a',
-        container: '#container', //内容替换的容器
-        show: 'fade',  //展现的动画，支持默认和fade, 可以自定义动画方式，这里为自定义的function即可。
-        cache: true,  //是否使用缓存
-        storage: true,  //是否使用本地存储
-        titleSuffix: '', //标题后缀
-        filter: function(){},
-        callback: function(){pajx_loadDuodsuo();}
-    })
+ $(document).pjax('a:not([nopjax])',   //jquery选择器，监听a元素，触发pjax。
+ '#container',                         //jquery选择器，存放页面内容的元素。
+ {scrollTo:false,                      //动作完成后不滚动
+ timeout:30000}); 
+$(document).on('pjax:send', function() {
+console.log('发送ajax请求')
+ });
+ $(document).on('pjax:complete', function() {
+pajx_loadDuodsuo()
+ 
+ });
 
 	</script>
 </body>
