1a5d32e3f3ac37d73f6733c7e6349e6b1b924ee6
疯狂减肥带 <ureygt@gmail.com>
2016-10-25 19:24:07 +0800

    Update header.php

--- header.php
+++ header.php
@@ -7,13 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="renderer" content="webkit">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <?php if (!is_pjax()) { ?>
     <title><?php $this->archiveTitle(array(
             'category'  =>  _t('分类 %s 下的文章'),
             'search'    =>  _t('包含关键字 %s 的文章'),
             'tag'       =>  _t('标签 %s 下的文章'),
             'author'    =>  _t('%s 发布的文章')
         ), '', ' - '); ?><?php $this->options->title(); ?></title>
-
+    <?php } ?>
     <!-- 使用url函数转换相关路径 -->
 
      <link rel="stylesheet" href="<?php $this->options->themeUrl('my.css'); ?>">
