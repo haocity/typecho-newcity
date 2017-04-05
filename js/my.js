f93a76f4d93d92c630369f4a66bb786d07649305
疯狂减肥带 <ureygt@gmail.com>
2017-04-05 20:35:22 +0800

    去除无用的翻页模块  添加表情插入(下个版本铺垫)

--- js/my.js
+++ js/my.js
@@ -8,22 +8,32 @@
         ele.remove();
     },4000)
     }
-function pagenumber() {
-  var near = document.getElementsByClassName('post-near')[0];
-  if (near) {
-    var last = near.childNodes[1];
-    var next = near.childNodes[3];
-    var lasturl = last.getElementsByTagName("a")[0];
-    var nexturl = next.getElementsByTagName("a")[0];
-    if (!lasturl) {
-      last.style.display = 'none'
+
+function insertFace(id) {
+    //插入函数
+    var textname = document.getElementById('textarea');
+    var textid = textname;
+    //插入到name为message的第一个标签
+    //兼容IE
+    faceele = '<img src="https://t4.haotown.cn/face/tieba/32/' + id + '.png">';
+    if (document.selection) {
+        textid.focus();
+        sel = document.selection.createRange();
+        sel.text = faceele;
+        sel.select();
+    } else if (textid.selectionStart || textid.selectionStart == "0") {
+        var startPos = textid.selectionStart;
+        var endPos = textid.selectionEnd;
+        var restoreTop = textid.scrollTop;
+        textid.value = textid.value.substring(0, startPos) + faceele + textid.value.substring(endPos, textid.value.length);
+        if (restoreTop > 0) {
+            textid.scrollTop = restoreTop;
+        }
+        textid.focus();
+        textid.selectionStart = startPos + faceele.length;
+        textid.selectionEnd = startPos + faceele.length;
+    } else {
+        textid.value += faceele;
+        textid.focus();
     }
-    if (!nexturl) {
-      next.style.display = 'none'
-    }
-    return {
-      l: lasturl,
-      n: nexturl
-    }
-  }
 }
