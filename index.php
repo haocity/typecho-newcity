5d8bc5af02b4e423c9da2654698d55da789375cf
疯狂减肥带 <ureygt@gmail.com>
2017-04-17 19:48:30 +0800

    添加图标

--- index.php
+++ index.php
@@ -17,10 +17,11 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
         <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
 			<h2 class="post-title" itemprop="name headline"><a itemtype="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
 			<ul class="post-meta">
-				<li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
-				<li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('F j, Y'); ?></time></li>
-				<li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
-				<li itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
+				<li itemprop="author" itemscope itemtype="http://schema.org/Person">
+<span class="iconfont smallicon icon-user"></span><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
+				<li><span class="iconfont smallicon icon-date"></span><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('F j, Y'); ?></time></li>
+				<li><span class="iconfont smallicon icon-fenlei"></span><?php $this->category(','); ?></li>
+				<li itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><span class="iconfont smallicon icon-pinglun"></span><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
 			</ul>
             <div class="post-content" itemprop="articleBody">
     			<?php $this->content('- 阅读剩余部分 -'); ?>
