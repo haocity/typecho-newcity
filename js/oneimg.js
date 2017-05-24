90e9d6300f860b07a459ed8cb5d4bd5e1916cd48
皓东haocity <ureygt@gmail.com>
2017-05-24 20:29:10 +0800

    小优化一下

--- js/oneimg.js
+++ js/oneimg.js
@@ -1,10 +1,10 @@
-function $d(ele) {
-    return document.getElementById(ele);
+function $c(ele) {
+    return document.querySelector(ele);
 }
-$(document).ready(function() {
+window.addEventListener('load',function() {
     Oneimg = new Object();
     var warp = document.createElement("div");
-    var ele = '<div class="changerimg" onmouseenter="showoneimg()" onmouseleave="hideoneimg()" onclick="showoneimg()"><img  id="oneimgmenuimg" src="https://www.haotown.cn/usr/themes/newcity/img/imgmenu.png" style="width: 70px;"><div id="oneimgmenu"><div class="hcset"><p><input type="checkbox" id="hccolorswitch" onclick="hcswitch()" >背景颜色<input type="color" id="hccolor" value="#ffffff" /> </p><p id="hctmd" class="range">透明度 <input type="range" name="points" min="0" max="100"  id="hcsetrange" onmouseup="hcchangerrange()" onmouseout="hcchangerrange()" style="width: 70px;"/> </p></div><img src="https://www.haotown.cn/usr/themes/newcity/img/imgdown.png" title="下载/评论图片" style="width: 70px;"   onclick="DownImg()"><img src="https://www.haotown.cn/usr/themes/newcity/img/imgchanger.png" title="更换图片" style="width: 70px;" onclick="ChangerImg()"></div></div>';
+    var ele = '<div class="changerimg" onmouseenter="showoneimg()" onmouseleave="hideoneimg()" onclick="showoneimg()"><style class="oneimgcss" type="text/css"></style><img  id="oneimgmenuimg" src="https://www.haotown.cn/usr/themes/newcity/img/imgmenu.png" style="width: 70px;"><div id="oneimgmenu"><div class="hcset"><p><input type="checkbox" id="hccolorswitch" onclick="hcswitch()" >背景颜色<input type="color" id="hccolor" value="#ffffff" /> </p><p id="hctmd" class="range">透明度 <input type="range" name="points" min="0" max="100"  id="hcsetrange" onmouseup="hcchangerrange()" onmouseout="hcchangerrange()" style="width: 70px;"/> </p></div><img src="https://www.haotown.cn/usr/themes/newcity/img/imgdown.png" title="下载/评论图片" style="width: 70px;"   onclick="DownImg()"><img src="https://www.haotown.cn/usr/themes/newcity/img/imgchanger.png" title="更换图片" style="width: 70px;" onclick="ChangerImg()"></div></div>';
     warp.innerHTML = ele;
     document.body.appendChild(warp);
     Oneimg.oneimg = getCookie("oneimg");
@@ -15,36 +15,36 @@ $(document).ready(function() {
     if (Oneimg.oneimg == "") {
         Oneimg.postbg = 82;
         document.cookie = "postbg=" + Oneimg.postbg + ";path=/";
-        Oneimg.bgcolor = $d("hccolor").value;
+        Oneimg.bgcolor = $c("#hccolor").value;
         document.cookie = "bgcolor=" + Oneimg.bgcolor + ";path=/";
         Oneimg.oneimg = 2266;
         document.cookie = "oneimg=" + Oneimg.oneimg + ";path=/";
         Oneimg.bgcolorswitch = "false";
         document.cookie = "bgcolorswitch=" + Oneimg.bgcolorswitch + ";path=/";
     }
-    $d("hcsetrange").value = Oneimg.postbg;
+    $c("#hcsetrange").value = Oneimg.postbg;
     hcchangerrange();
     if (document.body.clientWidth <= 767) {
         setInterval("hcchangerrange()", 300);
     }
-    $d("hccolor").value = Oneimg.bgcolor;
+    $c("#hccolor").value = Oneimg.bgcolor;
     if (Oneimg.bgcolorswitch == "true") {
-        $d("hccolorswitch").checked = true;
+        $c("#hccolorswitch").checked = true;
         hcchangercolor();
     } else {
         Oneimg.url = "https://t4.haotown.cn/img/bj@" + Oneimg.oneimg + ".jpg";
         Oneimg.elebg.style.backgroundImage = "url(" + Oneimg.url + ")";
     }
-});
+},false)
 
 function showoneimg() {
-    $d("oneimgmenu").style.display = "block";
-    $d("oneimgmenuimg").style.display = "none";
+    $c("#oneimgmenu").style.display = "block";
+    $c("#oneimgmenuimg").style.display = "none";
 }
 
 function hideoneimg() {
-    $d("oneimgmenu").style.display = "none";
-    $d("oneimgmenuimg").style.display = "block";
+    $c("#oneimgmenu").style.display = "none";
+    $c("#oneimgmenuimg").style.display = "block";
     hcchangercolor();
 }
 
@@ -72,22 +72,22 @@ function DownImg() {
 }
 
 function hcchangercolor() {
-    Oneimg.bgcolor = $d("hccolor").value;
+    Oneimg.bgcolor = $c("#hccolor").value;
     document.cookie = "bgcolor=" + Oneimg.bgcolor + ";path=/";
     hcswitch();
 }
 
 function hcchangerrange() {
-    Oneimg.postbg = $d("hcsetrange").value;
+    Oneimg.postbg = $c("#hcsetrange").value;
     if (Oneimg.postbg < 1) {
         Oneimg.postbg == 0;
     }
-    $("body").append('<style  type="text/css">.post {background-color:rgba(255,255,255,' + Oneimg.postbg / 100 + ");}</style>");
+    $c(".oneimgcss").innerHTML='.post {background-color:rgba(255,255,255,' + Oneimg.postbg / 100 + ');';
     document.cookie = "postbg=" + Oneimg.postbg + ";path=/";
 }
 
 function hcswitch() {
-    var y = $d("hccolorswitch").checked;
+    var y = $c("#hccolorswitch").checked;
     if (y == true) {
         document.cookie = "bgcolorswitch=true;path=/";
         Oneimg.elebg.style.backgroundColor = Oneimg.bgcolor;
